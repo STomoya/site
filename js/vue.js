b3757d73be3e73b9ba596ad8e2e1b62e4c22ed65
STomoya <blackie0110@gmail.com>
2020-02-15 14:54:41 +0900

    [UPDATE] jsonify

--- js/vue.js
+++ js/vue.js
@@ -3,22 +3,7 @@ var news = new Vue({
     el: '#news',
     data: {
         lang: 'en',
-        news: [
-            {
-                year: 2020,
-                month: 02,
-                day: 12,
-                jtext: '卒業論文を提出しました.',
-                etext: 'I have submitted my graduation thesis.'
-            },
-            {
-                year: 2020,
-                month: 01,
-                day: 24,
-                jtext: 'IEEE ICCASP 2020 に論文が採択されました.',
-                etext: 'My paper was accepted at IEEE ICASSP 2020.'
-            }
-        ]
+        news: []
     },
     methods: {
         change_lang: function () {
@@ -29,34 +14,28 @@ var news = new Vue({
                 this.lang = 'en'
             }
         }
+    },
+    mounted () {
+        axios.get("https://stomoya.github.io/json/news.json").then(response => (this.news = response.data))
     }
 })
 
 var trend = new Vue({
     el: '#trends',
     data: {
-        trends: [
-            'ICASSP 2020',
-            'machine learning',
-            'docker',
-            'docker-compose',
-            'nvidia-docker',
-            'python',
-            'pytorch',
-            'Rust',
-        ]
+        trends: []
+    },
+    mounted () {
+        axios.get("https://stomoya.github.io/json/trends.json").then(response => (this.trends = response.data.trends))
     }
 })
 
 var achievement = new Vue({
     el: '#achievement',
     data: {
-        achievements: [
-            {
-                year: 2020,
-                month: 01,
-                text: 'T. Sawada, M. Katsurai, "A Deep Multimodal Approach for Map Image Classification", in proceeding of 45th International Conference on Acoustics, Speech, and Signal Processing (ICASSP), (accepted for publication)'
-            }
-        ]
+        achievements: []
+    },
+    mounted () {
+        axios.get("https://stomoya.github.io/json/achievements.json").then(response => (this.achievements = response.data))
     }
 })
