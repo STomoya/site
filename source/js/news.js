961d046a38d5a1cab9285805a65ebe2796f49e4b
tomoya <blackie0110@gmail.com>
2020-02-22 16:49:30 +0900

    [UPDATE] news.html

--- source/js/news.js
+++ source/js/news.js
@@ -4,9 +4,7 @@ var news = new Vue({
     data: {
         lang: 'en',
         news: [],
-        year: 0,
-        month: 0,
-        months: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        selectedDay: null,
     },
     methods: {
         change_lang: function () {
@@ -17,31 +15,21 @@ var news = new Vue({
                 this.lang = 'en'
             }
         },
+        dayClicked(day) {
+            this.selectedDay = day;
+        },
     },
     mounted () {
         axios.get("https://stomoya.github.io/source/json/news.json").then(response => (this.news = response.data));
-        var today = new Date();
-        this.year = today.getFullYear();
-        this.month = today.getMonth() + 1;
     },
     computed: {
-        years: function () {
-            var years = [];
-            for (index in this.news) {
-                if (!years.includes(this.news[index].year)) {
-                    years.push(this.news[index].year);
-                }
-            }
-            return years
+        attributes() {
+            return this.news.map(n => ({
+                key: `news`,
+                dot: true,
+                dates: new Date(n.year, n.month-1, n.day),
+                customData: n,
+            }));
         },
-        shows: function () {
-            var shows = [];
-            for (index in this.news) {
-                if (this.news[index].year == this.year && this.news[index].month == this.month) {
-                    shows.push(this.news[index])
-                }
-            }
-            return shows;
-        }
     },
 })
