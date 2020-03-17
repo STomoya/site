4ec545111b35fa2b2299660beb0b8eeca1e608d1
tomoya <blackie0110@gmail.com>
2020-03-17 12:13:08 +0900

    [ADD] projects page

---
+++ source/js/projects.js
@@ -0,0 +1,21 @@
+
+var projects = new Vue({
+    el: '#projects',
+    data: {
+        lang: 'en',
+        projects: [],
+    },
+    methods: {
+        change_lang: function () {
+            if (this.lang === 'en'){
+                this.lang = '日本語'
+            }
+            else {
+                this.lang = 'en'
+            }
+        },
+    },
+    mounted () {
+        axios.get("https://stomoya.github.io/source/json/projects.json").then(response => (this.projects = response.data));
+    },
+})
