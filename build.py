3993fec7640511e5a3cefcf22150e5da273a91c9
tomoya <blackie0110@gmail.com>
2020-03-18 13:43:42 +0900

    [UPDATE] build with one command

--- build.py
+++ build.py
@@ -12,7 +12,10 @@ pages = [
 env = Environment(loader=FileSystemLoader(base_folder, encoding='utf-8'))
 
 for page in pages:
+    print('building {}'.format(page))
     template = env.get_template(page)
     rendered = template.render()
     with open(build_folder+page, 'w', encoding='utf-8') as fout:
         fout.write(str(rendered))
+
+print('build complete')
