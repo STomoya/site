095e63773c5df5a07a07337f90d7920273f5da71
tomoya <blackie0110@gmail.com>
2020-03-18 12:27:48 +0900

    [ADD] building script

---
+++ build.py
@@ -0,0 +1,18 @@
+from jinja2 import Environment, FileSystemLoader
+
+base_folder = './templates'
+build_folder = './docs/'
+
+pages = [
+    'index.html',
+    'news.html',
+    'projects.html'
+]
+
+env = Environment(loader=FileSystemLoader(base_folder, encoding='utf-8'))
+
+for page in pages:
+    template = env.get_template(page)
+    rendered = template.render()
+    with open(build_folder+page, 'w', encoding='utf-8') as fout:
+        fout.write(str(rendered))
